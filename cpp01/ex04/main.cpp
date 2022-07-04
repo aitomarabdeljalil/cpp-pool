98895c67636e8170b61b5e0a52ea09093c1c70e6
aitomarabdeljalil <80470779+aitomarabdeljalil@users.noreply.github.com>
2022-07-04 20:23:41 +0100

    cpp01 ex05 v1

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,13 +6,14 @@
 /*   By: aait-oma <aait-oma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/03 11:40:22 by aait-oma          #+#    #+#             */
-/*   Updated: 2022/07/03 19:37:55 by aait-oma         ###   ########.fr       */
+/*   Updated: 2022/07/04 19:07:14 by aait-oma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <sstream>
 
 int main(int ac, char **av)
 {
@@ -28,6 +29,9 @@ int main(int ac, char **av)
 	std::fstream fileto;
 	std::string line;
 	int index;
+	int i = 0;
+	std::stringstream	buffer;
+	std::string			content;
 
 	fileto.open(filename + ".replace", std::ios::out);
 	if (!fileto)
@@ -40,15 +44,14 @@ int main(int ac, char **av)
 			std::cout << "Source file cant get open or doesnt exist" << std::endl;
 			return 0;
 		}
-		while (getline(file, line))
+		buffer << file.rdbuf();
+		content = buffer.str();
+		while ((index = (int) content.find(s1)) != std::string::npos)
 		{
-            while ((index = (int) line.find(s1)) != std::string::npos)
-			{
-				line.erase(index, s1.length);
-			}
-			
-			fileto << line << "\n";
-        }
+			content.erase(index, s1.length());
+			content.insert(index, s2);
+		}
+		fileto << content;
 	}
 	return 0;
 }
