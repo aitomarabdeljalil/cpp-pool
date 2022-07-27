8e1cca5c6b3552e09e12960a5a44728c437485ca
Abdeljalil Ait omar <aait-oma@e3r2p7.1337.ma>
2022-07-27 19:07:56 +0100

    cpp02 v1

--- cpp02/ex00/Fixed.hpp
+++ cpp02/ex00/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: aait-oma <aait-oma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/25 11:46:11 by aait-oma          #+#    #+#             */
-/*   Updated: 2022/07/25 18:43:45 by aait-oma         ###   ########.fr       */
+/*   Updated: 2022/07/27 18:04:37 by aait-oma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,16 +22,48 @@ private:
     static const int fractp = 8;
 public:
     Fixed();
+    Fixed(const Fixed& f);
+    Fixed& operator=(const Fixed& f);
     ~Fixed();
+    int getRawBite( void ) const;
+    void setRawBite(int const raw)
 };
 
-Fixed::Fixed(/* args */)
+Fixed::Fixed()
 {
+    fixe = 0;
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed& f)
+{
+    fixe = f.fixe;
+    fractp = f.fixe;
+    std::cout << "Copy constructor called" << std::endl;
 }
 
 Fixed::~Fixed()
 {
+    std::cout << "Destructor called" << std::endl;
 }
 
+int Fixed::getRawBite() const
+{
+
+    return this->fixe;
+}
+
+Fixed &Fixed::operator=(const Fixed &other)
+{
+    std::cout << "Copy assignment operator called" << std::endl;
+    if(this != &other)
+        this->fix_p = other.getRawBits();
+    return *this;   
+}
+
+void setRawBite(int const raw)
+{
+        
+}
 
 #endif
