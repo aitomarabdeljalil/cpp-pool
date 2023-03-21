48f04f6cabd61c5eb35f500da113d531c4942a71
Abdeljalil Ait omar <aait-oma@e2r2p12.1337.ma>
2023-03-21 11:00:28 +0000

    normal casses done

---
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aait-oma <aait-oma@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/21 10:43:01 by aait-oma          #+#    #+#             */
+/*   Updated: 2023/03/21 10:43:44 by aait-oma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "BitcoinExchange.hpp"
+
+std::string	trim(std::string s, std::string set)
+{
+    s.erase(0, s.find_first_not_of(set));
+    s.erase(s.find_last_not_of(set) + 1);
+    return (s);
+}
+
+std::list<std::string>	split(std::string s, std::string delimiter) {
+    std::list<std::string>	result;
+    size_t					pos = 0;
+    std::string				token;
+
+	if (delimiter.empty())
+		throw std::invalid_argument("delimiter cannot be an empty string.");
+    while ((pos = s.find(delimiter)) != std::string::npos) {
+        token = s.substr(0, pos);
+        result.push_back(token);
+        s.erase(0, pos + delimiter.length());
+    }
+    result.push_back(s);
+    return (result);
+}
